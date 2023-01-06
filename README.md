cce94353c6ce90c719f8220bacb553584ab0781d
ravaligadipi <112492749+ravaligadipi@users.noreply.github.com>
2023-01-06 19:29:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-tndpvu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-tndpvu)
---
+++ index.js
@@ -0,0 +1,21 @@
+// run `node index.js` in the terminal
+for (let i = 1; i <= 100; i++) {
+  // console.log(i);
+  if (i % (3 * 5) == 0) {
+    console.log(`${i} ==> Amazon`);
+    i = i + 1;
+  }
+
+  if (i % 3 == 0) {
+    console.log(`${i} ==> Google`);
+    i = i + 1;
+  }
+  if (i % 5 == 0) {
+    console.log(`${i} ==> Facebook`);
+    i = i + 1;
+  }
+
+  console.log(i);
+}
+
+// console.log(`Hello Node.js v${process.versions.node}!`);
